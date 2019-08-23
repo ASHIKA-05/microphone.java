b1a2e1e8e038858d4ab7b2a914f7c24ff8b2126a
ASHIKA-05 <53563752+ASHIKA-05@users.noreply.github.com>
2019-08-23 16:36:04 +0530

    Update microphone.java

--- microphone.java
+++ microphone.java
@@ -1,3 +1,16 @@
+
+
+
+Micro and Array Update
+Micro purchased an array A having N integer values. After playing it for a while, he got
+bored of it and decided to update value of its element. In one second he can increase
+value of each array element by 1. He wants each array element's value to become greater
+than or equal to K. Please help Micro to find out the minimum amount of time it will take,
+for him to do so.
+
+
+
+
 import java.util.*;
 
  
@@ -6,25 +19,7 @@ class TestClass {
 
     public static void main(String args[] ) throws Exception {
 
-        /*
-
-         * Read input from stdin and provide input before running
-
-         * Use either of these methods for input
-
- 
-
-        //BufferedReader
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String line = br.readLine();
-
-        int N = Integer.parseInt(line);
-
-*/
-
-        //Scanner
+       
 
       Scanner sc = new Scanner(System.in);
 
